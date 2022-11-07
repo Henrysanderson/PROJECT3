2599a6d5430137669568b39c0188daaf2efa3090
Henry Sanderson Viyuyi <henryviyuyi2@gmail.com>
2022-11-08 00:36:42 +0200

    Setting Up

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("base.html")
+    return render_template("base.html)")
 
 
 if __name__ == "__main__":
